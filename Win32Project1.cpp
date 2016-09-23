3ec9056c53b47732601f32789c4b7aadc34ba6b0
leminh96 <quocminh97@gmail.com>
2016-09-23 09:55:44 +0700

    Add files via upload

--- Win32Project1.cpp
+++ Win32Project1.cpp
@@ -5,7 +5,7 @@
 #include <winuser.h>
 #include <commctrl.h>
 #include <cstdlib>
-#include <ctime>
+
 
 #pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
 #pragma comment(lib, "ComCtl32.lib")
